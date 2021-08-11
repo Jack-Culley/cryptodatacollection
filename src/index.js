48b6445361df651ead6e8c2c749650c19bb85966
Jack-Culley <jcu2ley@gmail.com>
2021-08-11 13:32:39 -0400

    made useDataFetch hook to gather data on crypto api

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import useDataFetch from './useDataFetch';
 
 function Index() {
 
   return (
     <div>
       <App />
-      <nav>
-        <ul>
-          <li>Bitcoin</li>
-        </ul>
-      </nav>
+        {useDataFetch('https://api.coingecko.com/api/v3/coins/juggernaut?localization=true&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')}
     </div>
   ); 
 }
---
+++ src/useDataFetch.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import './index.css';
+
+function useDataFetch (url) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect (() => {
+        fetch(url) 
+        .then(response => {
+            if(response.ok){
+                return response.json()
+            }
+            throw response;
+        })
+        .then(data => setData(data))
+        .catch(error => {
+            console.error("Error fetching data: ", error);
+            setError(error);
+        })
+        .finally(() => {
+            setLoading(false);
+        })
+    }, []) 
+
+    if(loading) return "Loading...";
+    if(error) return "Error!";
+
+    return(
+        <div>
+            <ul>
+                <li>{data.name}{data.description.en}{data.market_data.current_price.usd}</li>
+            </ul>
+        </div>
+    );
+}
+
+export default useDataFetch;
